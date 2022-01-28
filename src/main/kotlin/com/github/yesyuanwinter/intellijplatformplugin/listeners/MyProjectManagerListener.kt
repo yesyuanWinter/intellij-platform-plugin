d0fa425dd8c9811fba86da3b36b29f4da0a503e6
GitHub Action <action@github.com>
2022-01-28 05:00:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/yesyuanwinter/intellijplatformplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yesyuanwinter/intellijplatformplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yesyuanwinter.intellijplatformplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yesyuanwinter.intellijplatformplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/yesyuanwinter/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yesyuanwinter.intellijplatformplugin.services
+
+import com.github.yesyuanwinter.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
